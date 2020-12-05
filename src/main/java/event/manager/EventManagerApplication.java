edebfb07461197b7c2eaa5480b35512ae616d052
v.drugov.stc <v.drugov.stc@innopolis.university>
2020-12-05 22:31:05 +0700

    Adding Spring Boot structure and Liquibase settings and basic examples

---
+++ src/main/java/event/manager/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package event.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+}
