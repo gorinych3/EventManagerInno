09c8aace91483dc2349183353914cd2323c62684
Divaev Artur <a.divaev.stc@innopolis.university>
2021-01-21 19:12:29 +0300

    1550 1) Поправлена ошибка доступа

--- src/main/java/ru/inno/projects/config/WebSecurityConfig.java
+++ src/main/java/ru/inno/projects/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**", "/registration", "/registration/invitation/*", "/activate/*", "/static/*").permitAll()
+                .antMatchers("/", "/registration", "/registration/invitation/*", "/activate/*", "/static/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
